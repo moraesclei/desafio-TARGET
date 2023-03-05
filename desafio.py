a367a863bd214744addc79848fd4ab2eebdb6060
Moraes Clei <moraesclei@gmail.com>
2023-03-05 18:26:55 -0300

    DESAFIO

---
+++ desafio.py
@@ -0,0 +1,40 @@
+
+# QUESTÃO 1
+"""Indice = 13
+Soma = 0
+K = 0
+
+K = K + 1
+Soma = Soma + K
+while K < Indice:
+    K = K + 1
+    Soma = Soma + K
+print(Soma)"""
+
+
+# QUESTÃO 2
+
+# QUESTÃO 3
+ # a) 9
+ # B) 128
+ # C) 49
+ # D) 100
+ # E) 16
+ # F) 200
+
+# QUESTÃO 5
+
+"""string = "exemplo de string"
+string_invertida = string[::-1]
+print(string_invertida)"""
+
+    
+ 
+
+
+    
+
+   
+    
+    
+    
